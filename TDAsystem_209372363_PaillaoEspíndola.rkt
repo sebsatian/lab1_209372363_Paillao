a7f02ac6d4628bae68e2b76ebdf2f1d9041cccd2
sebsatian <145939228+sebsatian@users.noreply.github.com>
2023-09-29 23:09:00 -0300

    Update TDAsystem_209372363_PaillaoEspíndola.rkt
    corrección TDA

--- TDAsystem_209372363_PaillaoEspíndola.rkt
+++ TDAsystem_209372363_PaillaoEspíndola.rkt
@@ -1,13 +1,21 @@
 #lang racket
 
-; TDA para sistema
-(define-struct system (chatbots))
-; Constructor para el TDA sistema
-(define (crear-sistema chatbots)
-  (make-sistema chatbots))
-; Selector para el TDA sistema
-(define (obtener-chatbots system)
-  (sistema-chatbots system))
-; Modificador para el TDA sistema
-(define (agregar-chatbot system chatbot)
-  (set-sistema-chatbots! system (cons chatbot (obtener-chatbots sistema))))
+; Representación
+; Un system es una lista que comienza con el símbolo 'system y contiene una lista de chatbots.
+; Ejemplo: '(system (chatbot1 chatbot2))
+
+; Constructor
+(define (create-system chatbots)
+  (list 'system chatbots))
+
+; Función de pertenencia
+(define (system? x)
+  (and (list? x) (eq? (car x) 'system)))
+
+; Selectores
+(define (get-chatbots system)
+  (cadr system))
+
+; Modificadores
+(define (add-chatbot system chatbot)
+  (set-cdr! system (cons chatbot (get-chatbots system))))
