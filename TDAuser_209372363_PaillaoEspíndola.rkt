d6820017cff16888259333079950dd4057aff56a
sebsatian <145939228+sebsatian@users.noreply.github.com>
2023-09-30 22:23:59 -0300

    Update TDAuser_209372363_PaillaoEspíndola.rkt

--- TDAuser_209372363_PaillaoEspíndola.rkt
+++ TDAuser_209372363_PaillaoEspíndola.rkt
@@ -1,15 +1,21 @@
-; TDA para user
-(define-struct user (nombre correo chatHistory))
-; Constructor para el TDA user
-(define (crear-user nombre correo chatHistory)
-  (make-user nombre correo chatHistory))
-; Selectores para el TDA user
-(define (obtener-nombre user)
-  (user-nombre user))
-(define (obtener-correo user)
-  (user-correo user))
-(define (obtener-chatHistory user)
-  (user-chatHistory user))
-; Modificador para el TDA user
-(define (agregar-chat user chat)
-  (set-user-chatHistory! user (cons chat (obtener-chatHistory user))))
+#lang racket
+
+; Representación
+; Un user es una lista que comienza con el símbolo 'user y contiene un nombre, correo y chatHistory.
+; Ejemplo: '(user "nombre" "correo" (chat1 chat2))
+
+; Constructor
+(define (create-user nombre correo chatHistory)
+  (list 'user nombre correo chatHistory))
+
+; Selectores
+(define (get-nombre user)
+  (cadr user))
+(define (get-correo user)
+  (caddr user))
+(define (get-chatHistory user)
+  (cadddr user))
+
+; Modificadores
+(define (add-chat user chat)
+  (set-cdddr! user (cons chat (get-chatHistory user))))
